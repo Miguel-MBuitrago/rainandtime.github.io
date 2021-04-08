e2fb7fb8ce077a5c169224726b5b118b11992126
Miguel <miguelmbfp@gmail.com>
2021-04-08 13:17:14 +0200

    merge rain and clock js refactaor

--- js/rain.js
+++ js/rain.js
@@ -1,4 +1,4 @@
-var c,ctx,rainDrops,timer;
+var canvas_rain,ctx_rain,rainDrops,timer_rain;
 
 window.addEventListener('DOMContentLoaded', ()=>{
   document.getElementsByName('rain')[0].addEventListener('click',setup)
@@ -17,19 +17,19 @@ class RainDrop{
   }
 
   show(){
-    ctx.beginPath();
-    ctx.strokeStyle = '#7bb8d3';
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x, this.y + this.l);
-    ctx.stroke();
+    ctx_rain.beginPath();
+    ctx_rain.strokeStyle = '#7bb8d3';
+    ctx_rain.moveTo(this.x, this.y);
+    ctx_rain.lineTo(this.x, this.y + this.l);
+    ctx_rain.stroke();
   }
 
   fall(){
     this.y += this.vy;
 
-    if(this.y > c.height){
-      this.x = Math.floor(Math.random() * (c.width + 10) ) - 5;
-      this.y = Math.floor(Math.random()* -c.height );
+    if(this.y > canvas_rain.height){
+      this.x = Math.floor(Math.random() * (canvas_rain.width + 10) ) - 5;
+      this.y = Math.floor(Math.random()* -canvas_rain.height );
       this.l = Math.floor(Math.random()* 20) + 1;
       this.vy = Math.floor(Math.random()* 10) + 4;
     }
@@ -38,9 +38,9 @@ class RainDrop{
 
 // Loop
 function loop(){
-  c.width=document.body.clientWidth;
-  c.height=document.body.clientHeight;
-  ctx.clearRect(0,0,c.width,c.height);
+  canvas_rain.width=document.body.clientWidth;
+  canvas_rain.height=document.body.clientHeight;
+  ctx_rain.clearRect(0,0,canvas_rain.width,canvas_rain.height);
 
   for(var n=0;n<rainDrops.length;n++ ){
     rainDrops[n].show();
@@ -50,31 +50,31 @@ function loop(){
 
 function setup(){
   if(document.getElementsByName('rain')[0].checked){
-    c = document.createElement("canvas");
+    canvas_rain = document.createElement("canvas");
 
-    document.body.insertBefore(c,document.getElementsByClassName('content')[0]);
+    document.body.insertBefore(canvas_rain,document.getElementsByClassName('content')[0]);
     //set the canvas width and height
-    c.width=document.body.clientWidth;
-    c.height=document.body.clientHeight;
+    canvas_rain.width=document.body.clientWidth;
+    canvas_rain.height=document.body.clientHeight;
 
-    c.id = 'rain';
-    ctx = c.getContext('2d');
+    canvas_rain.id = 'rain';
+    ctx_rain = canvas_rain.getContext('2d');
 
     // rain = new Rain(20,20,20,16);
     rainDrops = [];
 
     for(var i=0;i<500;i++){
       rainDrops[i] = new RainDrop(
-        Math.floor(Math.random() * (c.width + 10) ) - 5,
-        Math.floor(Math.random()* -c.height ),
+        Math.floor(Math.random() * (canvas_rain.width + 10) ) - 5,
+        Math.floor(Math.random()* -canvas_rain.height ),
         Math.floor(Math.random()* 20) + 1,
         Math.floor(Math.random()* 10) + 4
       );
     }
 
-    timer = setInterval(loop,10);
+    timer_rain = setInterval(loop,10);
   }else{
     document.getElementById('rain').remove()
-    clearInterval(timer)
+    clearInterval(timer_rain)
   }
 }
---
+++ js/reloj.js
@@ -0,0 +1,103 @@
+// window.addEventListener('DOMContentLoaded', setup, false)
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementsByName('clock_input')[0].addEventListener('click',init)
+  document.getElementsByName('clock_input')[0].checked = false;
+},false)
+
+var canvas_clock = document.createElement('canvas')
+canvas_clock.id = 'canvas_clock'
+var ctx_clock = canvas_clock.getContext('2d')
+var date = new Date()
+
+function drawDate(){
+  let h1 = document.createElement('h1')
+
+  if(document.getElementById('h1_date')){
+    document.getElementById('h1_date').remove()
+  }
+
+  h1.id = 'h1_date';
+
+  let day = date.getDate()
+  let month = date.getMonth() + 1
+  let year = date.getFullYear()
+
+  h1.innerText = day + ' / ' + month+ ' / ' + year
+
+  let div_clock = document.getElementById('clock')
+
+  div_clock.insertBefore(h1, div_clock.firstChild)
+}
+
+function init(){
+
+  if (document.getElementsByName('clock_input')[0].checked) {
+    canvas_clock.width = 200
+    canvas_clock.height = 200
+
+    ctx_clock.translate(100,100)
+    let div_clock = document.createElement('div')
+    div_clock.id = 'clock'
+    div_clock.appendChild(canvas_clock)
+
+    document.getElementsByTagName('main')[0].appendChild(div_clock)
+
+    drawClock()
+    setInterval(drawClock, 500)
+
+  }else {
+    document.getElementById('clock').remove()
+  }
+}
+
+function drawClock(){
+  ctx_clock.beginPath()
+  ctx_clock.fillStyle = 'white'
+  ctx_clock.arc(0,0,100,0,2*Math.PI)
+  ctx_clock.fill()
+
+  date = new Date();
+  drawDate();
+
+  let hour = date.getHours() % 12
+  let minute = date.getMinutes()
+  let second = date.getSeconds()
+
+  drawHand('s',second)
+  drawHand('m',minute)
+  drawHand('h',hour)
+}
+
+function drawHand(type,pos){
+  ctx_clock.beginPath()
+  ctx_clock.moveTo(0,0)
+
+  ctx_clock.lineWidth = 3
+  ctx_clock.lineCap = "round";
+
+  let deg,len
+  switch (type) {
+    case 'h':
+        deg = (Math.PI/6 * pos)
+        ctx_clock.strokeStyle = 'black'
+        len = 30
+      break;
+    case 'm':
+        deg = (Math.PI/30 * pos)
+        ctx_clock.strokeStyle = 'blue'
+        len = 60
+      break;
+    case 's':
+        deg = (Math.PI/30 * pos)
+        ctx_clock.strokeStyle = 'red'
+        len = 90
+      break;
+    default:
+  }
+
+  ctx_clock.rotate(deg)
+  ctx_clock.lineTo(0,-len)
+  ctx_clock.stroke()
+  ctx_clock.rotate(-deg)
+}
